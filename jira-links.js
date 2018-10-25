9bacd21042f717ac185550af79954c898d9fd4f4
Shaw Yu <syu@virtru.com>
2018-10-25 13:30:18 -0400

    Add simple replacement for PR titles

--- jira-links.js
+++ jira-links.js
@@ -1,10 +1,25 @@
 const issueTitles = document.getElementsByClassName('js-issue-title') || [];
-const issuePattern = /[a-zA-Z]{2,5}-[0-9]{1,5}/;
+const issuePattern = /([a-zA-Z]{2,5}-[0-9]{1,5})(.+)/;
 
 for(let titleNode of issueTitles) {
   const textContent = titleNode.textContent;
 
-  if(textContent.match(issuePattern)) {
-    console.log('Found match: ', textContent);
+  const matches = textContent.match(issuePattern);
+
+  if(matches) {
+    titleNode.textContent = '';
+
+    const ticketNumber = matches[1];
+    const restOfText = matches[2];
+
+    const ticketNumberNode = document.createTextNode(matches[1]);
+    const restOfTextNode = document.createTextNode(matches[2]);
+
+    let jiraLinkNode = document.createElement('a');
+    jiraLinkNode.href = `https://virtru.atlassian.net/browse/${ticketNumber}`;
+    jiraLinkNode.appendChild(ticketNumberNode);
+
+    titleNode.appendChild(jiraLinkNode);
+    titleNode.appendChild(restOfTextNode);
   }
 }
