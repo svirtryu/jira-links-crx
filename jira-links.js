5519787aea35af639f0d4b89f3d0e53aa6ee84c8
Shaw Yu <syu@virtru.com>
2018-10-25 13:36:36 -0400

    Poll page continuously for matching text

--- jira-links.js
+++ jira-links.js
@@ -1,25 +1,29 @@
 const issueTitles = document.getElementsByClassName('js-issue-title') || [];
 const issuePattern = /([a-zA-Z]{2,5}-[0-9]{1,5})(.+)/;
 
-for(let titleNode of issueTitles) {
-  const textContent = titleNode.textContent;
+const updateNodes = () => {
+  for(let titleNode of issueTitles) {
+    const textContent = titleNode.textContent;
 
-  const matches = textContent.match(issuePattern);
+    const matches = textContent.match(issuePattern);
 
-  if(matches) {
-    titleNode.textContent = '';
+    if(matches) {
+      titleNode.textContent = '';
 
-    const ticketNumber = matches[1];
-    const restOfText = matches[2];
+      const ticketNumber = matches[1];
+      const restOfText = matches[2];
 
-    const ticketNumberNode = document.createTextNode(matches[1]);
-    const restOfTextNode = document.createTextNode(matches[2]);
+      const restOfTextNode = document.createTextNode(restOfText);
 
-    let jiraLinkNode = document.createElement('a');
-    jiraLinkNode.href = `https://virtru.atlassian.net/browse/${ticketNumber}`;
-    jiraLinkNode.appendChild(ticketNumberNode);
+      let jiraLinkNode = document.createElement('a');
+      jiraLinkNode.href = `https://virtru.atlassian.net/browse/${ticketNumber}`;
+      const ticketNumberNode = document.createTextNode(ticketNumber);
+      jiraLinkNode.appendChild(ticketNumberNode);
 
-    titleNode.appendChild(jiraLinkNode);
-    titleNode.appendChild(restOfTextNode);
+      titleNode.appendChild(jiraLinkNode);
+      titleNode.appendChild(restOfTextNode);
+    }
   }
-}
+};
+
+setInterval(updateNodes, 1000);
