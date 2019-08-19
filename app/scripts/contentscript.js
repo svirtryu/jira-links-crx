32acb4504ea64c2942cd346114887a526f5fb9d2
Vladislav Baranchuk <kuguarpwnz@gmail.com>
2019-08-19 16:08:52 +0300

    Match all ticket numbers (#4)
    * Match all ticket numbers
    
    * Open link in the same tab

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,29 +1,15 @@
 const issueTitles = document.getElementsByClassName('js-issue-title') || [];
-const issuePattern = /([a-z]{2,}-\d+)(.*)/i;
+const issuePattern = /([a-z]{2,}-\d+)/gi;
 
 const updateNodes = () => {
-  for(let titleNode of issueTitles) {
-    const textContent = titleNode.textContent;
-
-    const matches = textContent.match(issuePattern);
-
-    if(matches) {
-      titleNode.textContent = '';
-
-      const ticketNumber = matches[1];
-      const restOfText = matches[2];
-
-      const restOfTextNode = document.createTextNode(restOfText);
-
-      let jiraLinkNode = document.createElement('a');
-      jiraLinkNode.href = `https://virtru.atlassian.net/browse/${ticketNumber}`;
-      const ticketNumberNode = document.createTextNode(ticketNumber);
-      jiraLinkNode.appendChild(ticketNumberNode);
-
-      titleNode.appendChild(jiraLinkNode);
-      titleNode.appendChild(restOfTextNode);
-    }
+  for (let titleNode of issueTitles) {
+    titleNode.innerHTML = titleNode.textContent.replace(issuePattern, (ticketNumber) => (
+      `<a href="https://virtru.atlassian.net/browse/${ticketNumber}">
+         ${ticketNumber}
+       </a>`
+    ));
   }
 };
 
+updateNodes();
 setInterval(updateNodes, 1000);
